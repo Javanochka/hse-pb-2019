7e28533d61c09f41bed41c858cc93cd06badaac3
Anna Nikiforovskaya <nikiannanik@gmail.com>
2019-11-15 12:44:56 +0300

    Added myself to facts.py

--- practice02/facts.py
+++ practice02/facts.py
@@ -7,6 +7,8 @@ Person = collections.namedtuple('Person', ['github_login', 'first_name', 'last_n
 people = [
     Person(github_login='example', first_name='Name', last_name='Surname',
            facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
+    Person(github_login='Javanochka', first_name="Anna", last_name='Nikiforovskaya',
+           facts=["I can purr like a cat.", "My second name is Maria.", "I hate pizza with pineapple."]),
 ]
 
 answers = {}
