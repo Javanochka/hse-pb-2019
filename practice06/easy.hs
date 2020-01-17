70be8b75a2115bd6d845a6a02019b3a1c68b8380
Anna Nikiforovskaya <nikiannanik@gmail.com>
2020-01-17 11:16:16 +0300

    Add haskell tasks

---
+++ practice06/easy.hs
@@ -0,0 +1,24 @@
+
+-- Your own gcd function!
+gcd' :: Int -> Int -> Int
+gcd' = undefined
+
+-- Conditional sum from 1 to n. If condition for the number is true, then add to the sum
+condSum :: (Int -> Bool) -> Int -> Int
+condSum = undefined
+
+-- Sum of digits
+sumOfDigits :: Int -> Int
+sumOfDigits = undefined
+
+-- Return sum and count of digits for the given n
+sum'n'count :: Int -> (Int, Int)
+sum'n'count n = undefined
+
+-- Make python-like zip for two lists. 
+-- For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
+ziplike :: [a] -> [b] -> [(a, b)]
+ziplike xs ys = undefined
+
+
+
---
+++ practice06/hard.hs
@@ -0,0 +1,1 @@
+-- Merge sort! You may need several functions
